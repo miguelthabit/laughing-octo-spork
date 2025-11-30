6282d643ba3e2c9a39b5c0a567dce17979cabd91
miguelthabit <miguelndilahomba@gmail.com>
2025-11-30 19:35:50 +0300

    fafa

--- cripsy.py
+++ cripsy.py
@@ -18,4 +18,25 @@ Player_h =15
 clock =pygame.time.Clock()
 def draw_ship(surface,x,y,w,h):
     points =[(x,y),(x+w,y),(x+w//2,y - h)]
-    pygame.draw.polygon(surface,points)
+    pygame.draw.polygon(surface,SHIP,points)
+
+    Cockpit_center =(x+w//2,y-h//3)
+    pygame.draw.circle(surface,TRIM,Cockpit_center,6)
+
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+            
+    screen.fill(BG)
+    draw_ship(screen,player_x - player_w//2,player_y,player_w,Player_h)
+
+    pygame.display.flip()
+    clock.tick(60)
+
+pygame.quit()
+sys.exit()
+
+
+    
